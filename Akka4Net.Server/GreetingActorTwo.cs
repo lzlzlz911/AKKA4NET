8a48f1cbc4be10d9743e589f02c555dba99ae7f1
lzlzlz911 <lzlzlz911@hotmail.com>
2016-03-23 16:52:55 +0800

    akka.remote

---
+++ Akka4Net.Server/GreetingActorTwo.cs
@@ -0,0 +1,20 @@
+﻿namespace Akka4Net.Server
+{
+    #region Using
+
+    using System;
+
+    using Akka.Actor;
+
+    using Akka4NET.Common;
+
+    #endregion
+
+    public class GreetingActorTwo : ReceiveActor
+    {
+        public GreetingActorTwo()
+        {
+            this.Receive<GreetingMessage>(greet => Console.WriteLine("ReceiveTwo"));
+        }
+    }
+}
